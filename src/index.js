a769de7d88a502b5f0314a05b92552a2e5d0ee03
Suraj Divate <surajdivate2020@gmail.com>
2024-10-19 10:50:17 +0530

    Set up databaase conncetion for the backend

--- src/index.js
+++ src/index.js
@@ -1,4 +1,26 @@
 import express from 'express';
+import dotenv from 'dotenv';
 
+dotenv.config({
+    path: "./env"
+})
 
 const app = express()
+import connectDB from './database/connection.js'
+
+connectDB()
+    .then(() => {
+        app.get('/', (req, res) => {
+            res.send(`<center><h1>Server is live!!🟢</h1></center>`)
+        })
+
+        app.listen(process.env.POST || 8000, () => {
+            console.log(`Sever is Live on ${process.env.PORT}`)
+        })
+    })
+    .catch((err) => {
+        console.log("MONGO DB Connection Failed!", err);
+    })
+
+
+
