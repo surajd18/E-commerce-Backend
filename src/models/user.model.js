3bb74bf4604970fc8787cb6bb3661bd5eddd18bd
Suraj Divate <surajdivate2020@gmail.com>
2024-10-22 12:14:04 +0530

    Log in feature

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 // Declare the Schema of the Mongo model
 const userSchema = new Schema({
@@ -26,11 +28,49 @@ const userSchema = new Schema({
         type: String,
         required: true,
     },
+    refreshToken: {
+        type: String,
+      },
 },
     {
         timestamps: true,
     }
 );
 
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+
+    const salt = await bcrypt.genSaltSync(10);
+    this.password = await bcrypt.hash(this.password, salt)
+})
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(this.password, password)
+}
+
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+
+    )
+}
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+
+    )
+}
 //Export the model
 export const User = mongoose.model('User', userSchema);
